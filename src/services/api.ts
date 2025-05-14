97e7f9b64bfb5d2df78fcc8df128d6ede9408d46
nistar <nistxrmavi@gmail.com>
2025-05-14 10:39:22 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -13,7 +13,8 @@ const YOUTUBE_API_KEYS = [
 'AIzaSyCvI9LPFjvOe3wOYcsGqhkK-kTJWJSBcKA',
 'AIzaSyDlqTNAKMjsfukzMUYZHRXshPgMYdMTXV4',
 'AIzaSyA9A2t73XXr7Ra9q1SpYcPDvHTozJMwmpE',
-'AIzaSyDfP0C-J3_8k_ynb03RIc-uSUiB_QoDkdI'
+'AIzaSyDfP0C-J3_8k_ynb03RIc-uSUiB_QoDkdI',
+'AIzaSyCS1H46t92_gfqpyQiUWjWyH5d2mvg12E0'
 ].filter(Boolean);
 
 let currentApiKeyIndex = 0;
