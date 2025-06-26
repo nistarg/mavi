48256becbd10a28e7084da1fa1f39049553333aa
nistar <nistxrmavi@gmail.com>
2025-06-26 11:36:36 +0530

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,9 +6,8 @@ import MovieCarousel from '../components/ui/MovieCarousel';
 const SUGGESTED_MOVIES = [
   'Yeh Jawaani Hai Deewani full movie',
   'ajab prem ki ghazab kahani full movie',
-  'jab we met full movie',
   'phir hera pheri full movie',
-  'dhamaal full movie'
+  'jab we met full movie'
 
   
 ];
