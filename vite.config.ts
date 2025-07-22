8eb8fbbb152d41f02fee8c03cc605566ac764064
nistar <nistxrmavi@gmail.com>
2025-07-22 15:48:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: './',
   plugins: [react()],
-  // ...other settings
+  build: {
+    // any other customizations you need
+  }
 });
